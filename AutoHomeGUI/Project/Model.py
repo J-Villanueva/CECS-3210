8576755d766322722476def3a47a416ffe109f18
Jared <j.villanueva511@outlook.com>
2020-05-20 06:59:56 -0400

    Project Set Up

---
+++ AutoHomeGUI/Project/Model.py
@@ -0,0 +1,5 @@
+# Model
+"""
+This file handles and executes the data of the Application
+Here is where data is processed at the request of the controller
+"""
---
+++ AutoHomeGUI/Project/View.py
@@ -0,0 +1,6 @@
+# Views
+"""
+This file manages the UI Interface of the Application
+Here is where events are made and sent
+"""
+from . import HomeScreen, LockScreen, Ambient, Security
