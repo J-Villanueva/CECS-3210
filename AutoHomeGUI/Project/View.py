d01d8e11f288ea675ea58006adcd80ddb0287d9a
Jared <j.villanueva511@outlook.com>
2020-05-22 12:03:59 -0400

    Window change testing
    check window functionality and running code from other file

--- AutoHomeGUI/Project/View.py
+++ AutoHomeGUI/Project/View.py
@@ -3,4 +3,38 @@
 This file manages the UI Interface of the Application
 Here is where events are made and sent
 """
-from . import HomeScreen, LockScreen, Ambient, Security
+from AdvProgProject2 import *
+
+if __name__ == "__main__":
+    import sys
+
+    end = False
+    while not end:
+        app = QtWidgets.QApplication(sys.argv)
+        lockScreen = QtWidgets.QWidget()
+        homeScreen = QtWidgets.QMainWindow()
+        ambient = QtWidgets.QWidget()
+        security = QtWidgets.QWidget()
+        settings = QtWidgets.QWidget()
+
+        ui = SmartHouse()
+
+        ui.setupUiLS(lockScreen)
+        # ui.retranslateUiLS()
+        # lockScreen.show()
+
+        ui.setupUiHS(homeScreen, ambient, security, settings)
+        ui.retranslateUiHS(homeScreen, ambient, security, settings)
+        homeScreen.show()
+
+        ui.setupUiAm(ambient, homeScreen)
+        ui.retranslateUiAm(ambient, homeScreen)
+
+        ui.setupUiSet(settings, homeScreen)
+        ui.retranslateUiSet(settings, homeScreen)
+
+        ui.setupUiSec(security, homeScreen)
+        ui.retranslateUiSec(security, homeScreen)
+
+        if sys.exit(app.exec_()):
+            end = True
